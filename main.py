a33b05052cc4078467d3b49ccc9b331d7d865257
Gonzalo Martone <gmartone@itba.edu.ar>
2023-08-10 14:17:47 -0300

    fix: code now finds solution

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from enum import Enum
+import copy
+from queue import PriorityQueue
 
 class Move(Enum):
     LEFT = 0
@@ -23,31 +25,35 @@ class Board:
         return self._matrix
 
     def can_move(self, move: Move) -> bool:
-        matix = self.getMatrix()
+        matrix = self.getMatrix()
         dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for i in range(len(matix)):
-            for j in range(len(matix[0])):
-                if(matix[i][j] == 0):
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if(matrix[i][j] == 0):
                     j += dir[move.value][0]
                     i += dir[move.value][1]
-                    return i >= 0 and i < len(matix) and j >= 0 and j < len(matix[0])
+                    return i >= 0 and i < len(matrix) and j >= 0 and j < len(matrix[0])
 
-        raise UnboundLocalError
+        raise RuntimeError("0 not found")
 
     def apply_move(self, move: Move):
-        matix = self.getMatrix()
+        matrix = self.getMatrix()
         dir = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-        for i in range(len(matix)):
-            for j in range(len(matix[0])):
-                if(matix[i][j] == 0):
-                    k = j + dir[move.value][0]
-                    l = i + dir[move.value][1]
-                    new_mat = matix[:] # generate a copy
-                    new_mat[i][j] = matix[k][l]
+        for i in range(len(matrix)):
+            for j in range(len(matrix[0])):
+                if(matrix[i][j] == 0):
+                    k = i + dir[move.value][1]
+                    l = j + dir[move.value][0]
+
+                    if (not (k >= 0 and k < len(matrix) and l >= 0 and l < len(matrix[0]))):
+                        raise Exception("Out of bounds")
+
+                    new_mat = copy.deepcopy(matrix)
+                    new_mat[i][j] = matrix[k][l]
                     new_mat[k][l] = 0
                     return Board(new_mat)
 
-        raise UnboundLocalError
+        raise RuntimeError("0 not found")
 
 class Node:
     def __init__(self, board: Board, parent = None):
@@ -61,15 +67,19 @@ class Node:
 #    def __hash__(self) -> int:
 #        return self._board.__hash__()
 
+def printMat(mat):
+    for row in mat:
+        print(row)
+
 def show_path(node: Node):
-    if(node.parent == None):
-        print(node.getBoard().getMatrix())
+    if(node.parent is None):
+        printMat(node.getBoard().getMatrix())
         return
     show_path(node.parent)
-    print(node.getBoard().getMatrix())
+    print()
+    printMat(node.getBoard().getMatrix())
 
-# dict of nodes
-nodes = {}
+seen = []
 
 solved_board = Board([
     [1, 2, 3],
@@ -80,21 +90,25 @@ solved_board = Board([
 start_board = Board([
     [5, 7, 3],
     [8, 2, 0],
-    [7, 6, 5],
+    [1, 6, 4],
 ])
 
-start_board = Board([
-    [1, 2, 3],
-    [8, 4, 0],
-    [7, 6, 5],
-])
+#start_board = Board([
+#    [1, 2, 3],
+#    [8, 4, 5],
+#    [7, 0, 6],
+#])
 
 root = Node(start_board)
 
-queue = [root]
+queue = PriorityQueue()
+i = 0
+queue.put((i, root))
+
 found = False
-while(len(queue) != 0):
-    current_node = queue.pop()
+while(not queue.empty()):
+    current_node = queue.get()[1]
+    # printMat(current_node.getBoard().getMatrix())
     current_node_board = current_node.getBoard()
     for move in Move:
         if(current_node_board.can_move(move)):
@@ -103,8 +117,12 @@ while(len(queue) != 0):
             if(board.getMatrix() == solved_board.getMatrix()):
                 found = True
                 show_path(new_node)
+                print(i)
                 exit(0)
             current_node.children.append(new_node)
-            queue.append(new_node)
+            if(board.getMatrix() not in seen):
+                i += 1
+                queue.put((i, new_node))
+            seen.append(board.getMatrix())
         current_node.children.append(None)
 

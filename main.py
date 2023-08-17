9dee384394edc69be22b7c63fd66f7baa4575a08
Santiago Ballerini <sballerini@itba.edu.ar>
2023-08-17 16:38:32 -0300

    style: fromat main.py

--- main.py
+++ main.py
@@ -9,31 +9,30 @@ from src.searching_algorithms.greedy import greedy
 
 
 def main():
-    config_file = 'config.json'
+    config_file = "config.json"
 
-    with open(config_file, 'r') as f:
+    with open(config_file, "r") as f:
         config = json.load(f)
 
-        levels_file = config['levels_file']
-        level = config['level']
-        
+        levels_file = config["levels_file"]
+        level = config["level"]
+
         sokoban = Sokoban(level, levels_file)
 
-        if config['user_play']:
+        if config["user_play"]:
             play(sokoban)
             exit(0)
 
-
         # TODO: Inject euristic function to searching algorithm
-        match config['searching_algorithm']:
-            case 'bfs':
+        match config["searching_algorithm"]:
+            case "bfs":
                 path_to_solution, elapsed_time = bfs(sokoban)
-            case 'dfs':
+            case "dfs":
                 path_to_solution, elapsed_time = dfs(sokoban)
-            case 'greedy':
+            case "greedy":
                 path_to_solution, elapsed_time = greedy(sokoban)
             case _:
-                raise Exception('Invalid searching algorithm')
+                raise Exception("Invalid searching algorithm")
 
         # Print solution
         print(f"Elapsed time: {elapsed_time} seconds")
@@ -41,5 +40,5 @@ def main():
             print(str(node))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()

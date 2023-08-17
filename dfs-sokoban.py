739c08ac7e03e6d2bcba743f73718a5b80681622
Juan Cruz Bafico <jbafico@itba.edu.ar>
2023-08-17 13:19:46 -0300

    feat: DFS implemented

--- dfs-sokoban.py
+++ dfs-sokoban.py
@@ -1,12 +1,10 @@
 from collections import defaultdict
 from sokoban import Sokoban
 import copy
-from queue import PriorityQueue
 from sokoban import NodeSokoban
 import time
 
-class BFS:
-
+class DFS:
     # Constructor
     def __init__(self):
         # Default dictionary to store graph
@@ -16,11 +14,11 @@ class BFS:
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
-    # Function to print a BFS of graph
-    def bfs(self, lvl: int):
+    # Function to print a DFS of graph
+    def dfs(self, lvl: int):
         start_time = time.time()
-        level = lvl
-        levels_file = "levels.txt"
+        level= lvl
+        levels_file= "levels.txt"
         sokoban = Sokoban(level, levels_file)
 
         first_state = copy.deepcopy(sokoban.get_level_state())
@@ -28,24 +26,26 @@ class BFS:
         boxes = copy.deepcopy(sokoban.get_boxes())
         goals = copy.deepcopy(sokoban.get_goals())
 
-        # Create a queue for BFS with the initial level_state as the first item in the queue
-        queue = PriorityQueue()
-        i = 0
-        current_node = NodeSokoban(first_state,player,boxes,goals)
-        queue.put((i, current_node))
+        # Create a set for DFS with the level state in the first item of the set
+        stack = []
+        i=0
+        current_node = NodeSokoban(first_state, player,boxes,goals)
+        stack.append((i, current_node))
 
-        # Create dictionary for parents so that I can reconstruct path
+        #Create the dictionary for parents so i can reconstruct path
         parents = {}
 
-        while queue and not sokoban.level_complete():
-            # Dequeue a level_state from queue and set it in sokoban instance
-            s_node = queue.get()[1]
+        while stack and not sokoban.level_complete():
+
+            s_node= stack.pop()[1]
             s_level_state = copy.deepcopy(s_node.get_level_state())
             player = copy.deepcopy(s_node.get_play())
             boxes = copy.deepcopy(s_node.get_boxes())
             goals = copy.deepcopy(s_node.get_goals())
-            
+
             sokoban.set_status(s_level_state,player,boxes,goals)
+            
+
             # Get all adjacent vertices of the dequeued vertex s.
             for direction in sokoban.get_valid_directions():
                 if sokoban.can_move(direction) or sokoban.can_push(direction):
@@ -53,23 +53,19 @@ class BFS:
                     current_node = NodeSokoban(copy.deepcopy(sokoban.get_level_state()),copy.deepcopy(sokoban.get_play()),copy.deepcopy(sokoban.get_boxes()),copy.deepcopy(sokoban.get_goals()))
                     if current_node not in parents:
                         i += 1
-                        #print(i)
-                        queue.put((i, current_node))
+                        print(i)
+                        stack.append((i, current_node))
                         parents[current_node] = s_node
                         if sokoban.level_complete():
                             break
-                    sokoban.set_status(s_level_state,player,boxes,goals)  # We go back to the parent state we were evaluating
-                    
+                    sokoban.set_status(s_level_state,player,boxes,goals)
         
-
         end_time = time.time()
         elapsed_time = end_time - start_time
         print(f"Elapsed time: {elapsed_time} seconds")
         for node in reconstruct_path(parents, current_node, NodeSokoban(first_state,player,boxes,goals)):
             print(str(node))
 
-
-   
 
 def reconstruct_path(parents, target, start):
     path = []
@@ -81,6 +77,5 @@ def reconstruct_path(parents, target, start):
     path.reverse()
     return path
 
-
-bfs = BFS()
-bfs.bfs(54)
+dfs = DFS()
+dfs.dfs(54)

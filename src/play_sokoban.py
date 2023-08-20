126b93a57cdd812838f04cd55a82e7a10085a760
Gonzalo Martone <gmartone@itba.edu.ar>
2023-08-20 00:43:35 -0300

    refactor: ifelse -> match case

--- src/play_sokoban.py
+++ src/play_sokoban.py
@@ -1,25 +1,27 @@
 from src.sokoban import Sokoban
 
+
 def play(sokoban: Sokoban):
     while (not sokoban.level_complete() and not sokoban.level_failed()):
         valid = False
         dir = Sokoban.Direction.NONE
         while (not valid):
-            print(sokoban) 
+            print(sokoban)
             key = input("wasdq:")
             valid = True
-            if key == "w":
-                dir = Sokoban.Direction.UP
-            elif key == "a":
-                dir = Sokoban.Direction.LEFT
-            elif key == "s":
-                dir = Sokoban.Direction.DOWN
-            elif key == "d":
-                dir = Sokoban.Direction.RIGHT
-            elif key == "q":
-                return
-            else:
-                valid = False
+            match key:
+                case "w":
+                    dir = Sokoban.Direction.UP
+                case "a":
+                    dir = Sokoban.Direction.LEFT
+                case "s":
+                    dir = Sokoban.Direction.DOWN
+                case "d":
+                    dir = Sokoban.Direction.RIGHT
+                case "q":
+                    return
+                case _:
+                    valid = False
             if (valid and sokoban.can_move(dir) or sokoban.can_push(dir)):
                 sokoban.move_player(dir)
 
